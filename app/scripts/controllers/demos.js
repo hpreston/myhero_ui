21a041db8e7eda45f0ae8e849d6448e6d920526e
Hank Preston <hank.preston@gmail.com>
2016-08-18 22:43:45 -0400

    Updated demos page to include Tropo.

--- app/scripts/controllers/demos.js
+++ app/scripts/controllers/demos.js
@@ -14,6 +14,13 @@ angular.module('myHeroApp')
                 });
                 window.location = "#/results";
             };
+            $scope.tropomsg = function() {
+                $http({
+                    method: 'GET',
+                    url: config.apiTropo + '/hello/' + $scope.mobilenumber
+                });
+                window.location = "#/results";
+            };
         }
     );
 
